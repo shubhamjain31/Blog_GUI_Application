5a63d1dd52b0a8505186a9c762ac534e8e79c5ad
shubhamjain31 <sj27754@gmail.com>
2021-07-17 22:48:05 +0530

    Added delete and update functonality using ElectronJS

--- src/ui/app.js
+++ src/ui/app.js
@@ -4,6 +4,8 @@ const description 	= document.getElementById('description')
 
 const articles 		= document.getElementById('articles')
 
+let articleId;
+
 const insertData = (newData) => {
 	fetch('http://localhost:5000/add',{
 		method:"POST",
@@ -14,7 +16,7 @@ const insertData = (newData) => {
 	})
 	.then(resp 	 => resp.json())
 	.then((data) => {
-		console.log(data)
+		getAllData();
 	})
 	.catch(error => console.log(error))
 }
@@ -40,11 +42,60 @@ function renderArticles(mydata){
 									<h5>${data.date}</h5>
 
 									<p>
-									<button type="button" class="btn btn-success">Update</button>
-									<button type="button" class="btn btn-danger">Delete</button>
+									<button type="button" class="btn btn-success" onclick="getDataById('${data.id}')">Update</button>
+									<button type="button" class="btn btn-danger" onclick="deleteData('${data.id}')">Delete</button>
 									</p>
 									</div>`
 	})
+}
+
+const deleteData = (id) => {
+	fetch(`http://localhost:5000/delete/${id}/`,{
+		method:"DELETE",
+		headers:{
+			'Content-type':'application/json'
+		}
+	})
+	.then((data) => {
+		getAllData();
+	})
+}
+
+const getDataById = (id) => {
+	fetch(`http://localhost:5000/get/${id}/`,{
+		method:"GET",
+		headers:{
+			'Content-type':'application/json'
+		}
+	})
+	.then(resp 	 => resp.json())
+	.then((data) => {
+		// getAllData();
+		renderOneItem(data);
+	})
+	.catch(error => console.log(error))
+}
+
+const renderOneItem = (mydata) => {
+	title.value 		= mydata.title
+	description.value 	= mydata.description
+
+	articleId = mydata.id
+}
+
+const updteData = (articleId, mydata) => {
+	fetch(`http://localhost:5000/update/${articleId}/`,{
+		method:"PUT",
+		headers:{
+			'Content-type':'application/json'
+		},
+		body:JSON.stringify(mydata)
+	})
+	.then(resp 	 => resp.json())
+	.then(() => {
+		getAllData();
+	})
+	.catch(error => console.log(error))
 }
 
 myform.addEventListener('submit', (e) =>{
@@ -55,7 +106,14 @@ myform.addEventListener('submit', (e) =>{
 		description: 	description.value
 	}
 
-	insertData(newData);
+	if(articleId){
+		updteData(articleId, newData);
+	}
+	else{
+		insertData(newData);
+	}
+
+	myform.reset();
 })
 
 getAllData();
